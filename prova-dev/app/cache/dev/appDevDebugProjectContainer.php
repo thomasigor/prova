4d293c1b3e767ce4f4bb5295c4c7951435769aba
thomasigor <thomasigor10@gmail.com>
2013-10-02 10:36:08 -0300

    prova ultimo
    criado o database e o squema com a entidade

--- prova-dev/app/cache/dev/appDevDebugProjectContainer.php
+++ prova-dev/app/cache/dev/appDevDebugProjectContainer.php
@@ -532,7 +532,7 @@ class appDevDebugProjectContainer extends Container
         $b = new \Doctrine\DBAL\Configuration();
         $b->setSQLLogger($a);
 
-        return $this->services['doctrine.dbal.default_connection'] = $this->get('doctrine.dbal.connection_factory')->createConnection(array('dbname' => 'symfony', 'host' => '127.0.0.1', 'port' => NULL, 'user' => 'root', 'password' => NULL, 'charset' => 'UTF8', 'driver' => 'pdo_mysql', 'driverOptions' => array()), $b, new \Symfony\Bridge\Doctrine\ContainerAwareEventManager($this), array());
+        return $this->services['doctrine.dbal.default_connection'] = $this->get('doctrine.dbal.connection_factory')->createConnection(array('dbname' => 'pessoas', 'host' => '127.0.0.1', 'port' => NULL, 'user' => 'root', 'password' => NULL, 'charset' => 'UTF8', 'driver' => 'pdo_mysql', 'driverOptions' => array()), $b, new \Symfony\Bridge\Doctrine\ContainerAwareEventManager($this), array());
     }
 
     /**
@@ -554,20 +554,23 @@ class appDevDebugProjectContainer extends Container
         $c = new \Doctrine\Common\Cache\ArrayCache();
         $c->setNamespace('sf2orm_default_9dce44896107f16eb636eedb7de4b77d');
 
-        $d = new \Doctrine\ORM\Configuration();
-        $d->setEntityNamespaces(array());
-        $d->setMetadataCacheImpl($a);
-        $d->setQueryCacheImpl($b);
-        $d->setResultCacheImpl($c);
-        $d->setMetadataDriverImpl(new \Doctrine\ORM\Mapping\Driver\DriverChain());
-        $d->setProxyDir('C:/wamp/www/prova-dev/app/cache/dev/doctrine/orm/Proxies');
-        $d->setProxyNamespace('Proxies');
-        $d->setAutoGenerateProxyClasses(true);
-        $d->setClassMetadataFactoryName('Doctrine\\ORM\\Mapping\\ClassMetadataFactory');
-        $d->setDefaultRepositoryClassName('Doctrine\\ORM\\EntityRepository');
-        $d->setNamingStrategy(new \Doctrine\ORM\Mapping\DefaultNamingStrategy());
+        $d = new \Doctrine\ORM\Mapping\Driver\DriverChain();
+        $d->addDriver(new \Doctrine\ORM\Mapping\Driver\AnnotationDriver($this->get('annotation_reader'), array(0 => 'C:\\wamp\\www\\prova-dev\\src\\Usuario\\Bundle\\UsuarioBundle\\Entity')), 'Usuario\\Bundle\\UsuarioBundle\\Entity');
 
-        $this->services['doctrine.orm.default_entity_manager'] = $instance = call_user_func(array('Doctrine\\ORM\\EntityManager', 'create'), $this->get('doctrine.dbal.default_connection'), $d);
+        $e = new \Doctrine\ORM\Configuration();
+        $e->setEntityNamespaces(array('UsuarioUsuarioBundle' => 'Usuario\\Bundle\\UsuarioBundle\\Entity'));
+        $e->setMetadataCacheImpl($a);
+        $e->setQueryCacheImpl($b);
+        $e->setResultCacheImpl($c);
+        $e->setMetadataDriverImpl($d);
+        $e->setProxyDir('C:/wamp/www/prova-dev/app/cache/dev/doctrine/orm/Proxies');
+        $e->setProxyNamespace('Proxies');
+        $e->setAutoGenerateProxyClasses(true);
+        $e->setClassMetadataFactoryName('Doctrine\\ORM\\Mapping\\ClassMetadataFactory');
+        $e->setDefaultRepositoryClassName('Doctrine\\ORM\\EntityRepository');
+        $e->setNamingStrategy(new \Doctrine\ORM\Mapping\DefaultNamingStrategy());
+
+        $this->services['doctrine.orm.default_entity_manager'] = $instance = call_user_func(array('Doctrine\\ORM\\EntityManager', 'create'), $this->get('doctrine.dbal.default_connection'), $e);
 
         $this->get('doctrine.orm.default_manager_configurator')->configure($instance);
 
@@ -3323,7 +3326,7 @@ class appDevDebugProjectContainer extends Container
             'database_driver' => 'pdo_mysql',
             'database_host' => '127.0.0.1',
             'database_port' => NULL,
-            'database_name' => 'symfony',
+            'database_name' => 'pessoas',
             'database_user' => 'root',
             'database_password' => NULL,
             'mailer_transport' => 'smtp',
--- prova-dev/src/Usuario/Bundle/UsuarioBundle/Entity/Pessoas.php
+++ prova-dev/src/Usuario/Bundle/UsuarioBundle/Entity/Pessoas.php
@@ -5,12 +5,12 @@ namespace Usuario\Bundle\UsuarioBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- *  Pessoas
+ *  pessoas
  *
  * @ORM\Table()
- * @ORM\Entity(repositoryClass="Usuario\Bundle\UsuarioBundle\Entity\ PessoasRepository")
+ * @ORM\Entity(repositoryClass="Usuario\Bundle\UsuarioBundle\Entity\ pessoasRepository")
  */
-class  Pessoas
+class  pessoas
 {
     /**
      * @var integer
@@ -20,13 +20,6 @@ class  Pessoas
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name=""nome: text email: text telefone: integer"", type="string", length=255)
-     */
-    private $"nome:TextEmail:TextTelefone:Integer";
 
     /**
      * @var string
@@ -61,33 +54,10 @@ class  Pessoas
     }
 
     /**
-     * Set "nome:TextEmail:TextTelefone:Integer"
-     *
-     * @param string $"nome:TextEmail:TextTelefone:Integer"
-     * @return  Pessoas
-     */
-    public function set"nome:TextEmail:TextTelefone:Integer"($"nome:TextEmail:TextTelefone:Integer")
-    {
-        $this->"nome:TextEmail:TextTelefone:Integer" = $"nome:TextEmail:TextTelefone:Integer";
-    
-        return $this;
-    }
-
-    /**
-     * Get "nome:TextEmail:TextTelefone:Integer"
-     *
-     * @return string 
-     */
-    public function get"nome:TextEmail:TextTelefone:Integer"()
-    {
-        return $this->"nome:TextEmail:TextTelefone:Integer";
-    }
-
-    /**
      * Set nome
      *
      * @param string $nome
-     * @return  Pessoas
+     * @return  pessoas
      */
     public function setNome($nome)
     {
@@ -110,7 +80,7 @@ class  Pessoas
      * Set email
      *
      * @param string $email
-     * @return  Pessoas
+     * @return  pessoas
      */
     public function setEmail($email)
     {
@@ -133,7 +103,7 @@ class  Pessoas
      * Set telefone
      *
      * @param integer $telefone
-     * @return  Pessoas
+     * @return  pessoas
      */
     public function setTelefone($telefone)
     {
